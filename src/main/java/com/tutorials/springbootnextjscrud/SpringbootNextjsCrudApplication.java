1c1fbf9b925717f18b9625d0a10066b4cdd95086
Christian Pelayo <pelayochristian.dev@gmail.com>
2020-08-21 16:56:16 +0800

    first commit

---
+++ src/main/java/com/tutorials/springbootnextjscrud/SpringbootNextjsCrudApplication.java
@@ -0,0 +1,13 @@
+package com.tutorials.springbootnextjscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootNextjsCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootNextjsCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorials/springbootnextjscrud/SpringbootNextjsCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorials.springbootnextjscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootNextjsCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
